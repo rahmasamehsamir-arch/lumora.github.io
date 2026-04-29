c41fb4afb577a94414c1edc7eb463da517b56c05
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-04-29 21:51:47 +0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,7 @@
-const profileBtn = document.getElementById('profileBtn');
-const dropdownMenu = document.getElementById('dropdownMenu');
+function openMenu() {
+  document.getElementById("sideWindow").style.right = "0";
+}
 
-// When the user clicks the button...
-profileBtn.addEventListener('click', () => {
-    // Toggle the "show" class
-    dropdownMenu.classList.toggle('show');
-});
-
-// Optional: Close the menu if the user clicks anywhere else on the screen
-window.addEventListener('click', (event) => {
-    if (!profileBtn.contains(event.target)) {
-        dropdownMenu.classList.remove('show');
-    }
-});
+function closeMenu() {
+  document.getElementById("sideWindow").style.right = "-260px";
+}

078eb9a6333fced76283c9525e65acdced645d07
Jana Safwat <janasafwat001@gmail.com>
2026-05-01 15:00:44 +0300

    update

--- javascript/index.js
+++ javascript/index.js
@@ -6,29 +6,3 @@ function closeMenu() {
   document.getElementById("sideWindow").style.right = "-260px";
 }
 
-
-
-
-
-
-
-let reviews = document.querySelectorAll(".review");
-let index = 0;
-
-reviews[index].classList.add("active");
-
-function changeReview(step) {
-    reviews[index].classList.remove("active");
-
-    index = index + step;
-
-    if (index >= reviews.length) {
-        index = 0;
-    }
-
-    if (index < 0) {
-        index = reviews.length - 1;
-    }
-
-    reviews[index].classList.add("active");
-}

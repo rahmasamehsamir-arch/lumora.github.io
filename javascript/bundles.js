611d4dc547104fb65c9593c62a7253ffce932e39
Jana ELsayed <janahassan057@gmail.com>
2026-05-04 21:27:25 +0300

    bundles.js

--- javascript/bundles.js
+++ javascript/bundles.js
@@ -20,89 +20,29 @@ allProducts.forEach(product => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-
-  
-  document.querySelectorAll(".info").forEach(infoBox => {
-
-    let minus = infoBox.querySelector(".minus");
-    let plus = infoBox.querySelector(".plus");
-    let count = infoBox.querySelector(".count");
-
-    let qty = 1;
-
-    plus.addEventListener("click", () => {
-      qty++;
-      count.innerText = qty;
-    });
-
-    minus.addEventListener("click", () => {
-      if (qty > 1) {
-        qty--;
-        count.innerText = qty;
-      }
-    });
-
-  });
-
-
-  document.querySelectorAll(".buttons .one button").forEach(btn => {
-
-    btn.addEventListener("click", () => {
-
-      let card = btn.closest(".info");
-
-      let name = card.querySelector(".product h2").innerText;
-      let price = parseFloat(
-        card.querySelector(".price h3").innerText.replace("EGP", "").trim()
-      );
-
-      let image = btn.closest("div[class]").querySelector("img").src;
-
-      let quantity = parseInt(card.querySelector(".count").innerText);
-
-      let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-      let existing = cart.find(item => item.name === name);
-
-      if (existing) {
-        existing.quantity += quantity;
-      } else {
-        cart.push({
-          id: Date.now(),
-          name,
-          price,
-          image,
-          quantity
-        });
-      }
-
-      localStorage.setItem("cart", JSON.stringify(cart));
-
-      alert("Added to cart!");
-    });
-
-  });
-
-});
 let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
 
 let buttons = document.querySelectorAll(".js");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", function () {
-    let productCard = this.closest("div");
+
+   
+    let productCard = this.closest(".lavender, .mocha, .rose, .strawberry, .lavenrose, .strawmocha");
 
     let product = {
-      name: productCard.querySelector("h2")?.innerText,
-      price: productCard.querySelector(".price h3")?.innerText,
-      img: productCard.querySelector("img")?.getAttribute("src")
+      name: productCard.querySelector(".product h2")?.innerText || "No name",
+      price: productCard.querySelector(".price h3")?.innerText || "No price",
+      img: productCard.querySelector("img")?.getAttribute("src") || ""
     };
 
     wishlist.push(product);
 
     localStorage.setItem("wishlist", JSON.stringify(wishlist));
 
+    console.log(product); 
+
     alert("Added to wishlist ❤️");
   });
 });
+

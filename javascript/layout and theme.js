04c927aa2436a3a3dd5d827a414eeb8fdebbd259
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 11:39:57 +0300

    Update layout and theme.js

--- javascript/layout and theme.js
+++ javascript/layout and theme.js
@@ -1,1 +1,12 @@
 
+const btn = document.querySelector(".mode");
+const body = document.body;
+if(localStorage.getItem("mode")==="dark"){
+  body.classList.add("dark);
+                     }
+btn.addEventListener("click,()=>{
+                     body.class.toggle("dark");
+if (body.classList.toggle("dark")){
+  localStorage.setItem("mode","light");
+}
+});

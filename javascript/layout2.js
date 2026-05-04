323e5a30f134262064ccf8be2cbf1e74c83d39c4
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 14:28:56 +0300

    Update layout2.js

--- javascript/layout2.js
+++ javascript/layout2.js
@@ -1,1 +1,20 @@
 
+const layoutBtn = document.querySelector(".layout-btn");
+
+
+if (localStorage.getItem("layout") === "vertical") {
+    document.body.classList.add("vertical");
+}
+
+
+if (layoutBtn) {
+    layoutBtn.addEventListener("click", () => {
+        document.body.classList.toggle("vertical");
+
+        if (document.body.classList.contains("vertical")) {
+            localStorage.setItem("layout", "vertical");
+        } else {
+            localStorage.setItem("layout", "horizontal");
+        }
+    });
+}

9635eaaf2b2860b37f9084a85b009947cd19ff69
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 18:53:33 +0300

    Implement layout toggle feature in layout.js
    Added layout toggle functionality to switch between vertical and horizontal layouts.

--- javascript/layout.js
+++ javascript/layout.js
@@ -2,23 +2,41 @@
 
 
 
-const btn = document.querySelector(".mode");
 const body = document.body;
+const modeBtn = document.querySelector(".mode");
+const layoutBtn = document.querySelector(".layout-btn");
 
 
 if (localStorage.getItem("mode") === "dark") {
     body.classList.add("dark");
 }
 
+if (localStorage.getItem("layout") === "vertical") {
+    body.classList.add("vertical");
+}
 
-if (btn) {
-    btn.addEventListener("click", () => {
+
+if (modeBtn) {
+    modeBtn.addEventListener("click", () => {
         body.classList.toggle("dark");
 
         if (body.classList.contains("dark")) {
             localStorage.setItem("mode", "dark");
         } else {
             localStorage.setItem("mode", "light");
+        }
+    });
+}
+
+
+if (layoutBtn) {
+    layoutBtn.addEventListener("click", () => {
+        body.classList.toggle("vertical");
+
+        if (body.classList.contains("vertical")) {
+            localStorage.setItem("layout", "vertical");
+        } else {
+            localStorage.setItem("layout", "horizontal");
         }
     });
 }

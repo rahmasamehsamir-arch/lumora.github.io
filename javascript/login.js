cecceff53fa2f488aa5ab11a96068f5751498b85
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-05 02:20:01 +0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -32,7 +32,7 @@ form.addEventListener("submit", function(e){
     if(user_data.password === password){
         alert('Welcome back ' + user_data.first_name);
         localStorage.setItem('logged', email);
-        window.location.href = "/index.html";
+        window.location.href = "../index.html";
     } else {
         alert('Incorrect password');
     }

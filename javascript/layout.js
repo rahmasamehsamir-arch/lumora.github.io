a2303f1d0c3e968b8d4926c0c10f533150220e92
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 13:50:45 +0300

    Update layout.js

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,23 +1,24 @@
 
 
+
+
 const btn = document.querySelector(".mode");
 const body = document.body;
 
 
-if(localStorage.getItem("mode") === "dark") {
+if (localStorage.getItem("mode") === "dark") {
     body.classList.add("dark");
 }
 
 
-btn.addEventListener("click", () => {
-    body.classList.toggle("dark");
+if (btn) {
+    btn.addEventListener("click", () => {
+        body.classList.toggle("dark");
 
- 
-    if(body.classList.contains("dark")) {
-        localStorage.setItem("mode", "dark");
-    } else {
-        localStorage.setItem("mode", "light");
-    }
-});
-
-
+        if (body.classList.contains("dark")) {
+            localStorage.setItem("mode", "dark");
+        } else {
+            localStorage.setItem("mode", "light");
+        }
+    });
+}

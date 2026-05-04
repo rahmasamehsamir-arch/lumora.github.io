691d24c4c0352bb3308b38b3f9d6d42270f0e5e8
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 11:57:58 +0300

    Update layout.js

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,16 +1,22 @@
 
+
 const btn = document.querySelector(".mode");
 const body = document.body;
-if(localStorage.getItem("mode") ==="dark"){
-  body.classList.add("dark);
-                     }
-btn.addEventListener("click, () => {
-                     body.classList.toggle("dark");
-if (body.classList.contains("dark")){
-  localStorage.setItem("mode","dark");
-} else{
-  localStorage.setItem("mode","light");
-}
-});
 
+// load saved mode
+if(localStorage.getItem("mode") === "dark") {
+    body.classList.add("dark");
+}
+
+// toggle on click
+btn.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    // save mode
+    if(body.classList.contains("dark")) {
+        localStorage.setItem("mode", "dark");
+    } else {
+        localStorage.setItem("mode", "light");
+    }
+});
 

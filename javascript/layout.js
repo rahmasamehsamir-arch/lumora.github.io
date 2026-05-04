18abc05fe4e411cbf82750e3c50413e4d8942b2c
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 11:49:14 +0300

    Update and rename layout and theme.js to layout.js

---
+++ javascript/layout.js
@@ -0,0 +1,16 @@
+
+const btn = document.querySelector(".mode");
+const body = document.body;
+if(localStorage.getItem("mode") ==="dark"){
+  body.classList.add("dark);
+                     }
+btn.addEventListener("click, () => {
+                     body.classList.toggle("dark");
+if (body.classList.contains("dark")){
+  localStorage.setItem("mode","dark");
+} else{
+  localStorage.setItem("mode","light");
+}
+});
+
+

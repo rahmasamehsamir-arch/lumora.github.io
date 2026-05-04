176d38eca11adc2fc33f87b92b36d6e432a1c51a
Jana ELsayed <janahassan057@gmail.com>
2026-05-05 01:40:17 +0300

    bundles.js

--- javascript/bundles.js
+++ javascript/bundles.js
@@ -53,23 +53,3 @@ document.querySelectorAll(".section > div").forEach((productCard) => {
     alert("Added to cart.");
   });
 });
-
-let wishlistButtons = document.querySelectorAll(".wishlist-button");
-wishlistButtons.forEach(function(btn) {
-    btn.addEventListener("click", function() {
-        let card = btn.closest(".info");
-        let product = {
-            name: card.querySelector("h2").innerText,
-            price: card.querySelector(".price h3").innerText,
-            image: card.parentElement.querySelector("img").src
-        };
-        let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
-        let exists = wishlist.some(function(item) {
-            return item.name === product.name;
-        });
-        if (!exists) {
-            wishlist.push(product);
-            localStorage.setItem("wishlist", JSON.stringify(wishlist));
-        }
-    });
-});

65921e36906612ff3ea0a8a4517c7e6c4c61a973
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-04 14:29:19 +0300

    Refactor dark mode toggle functionality

--- javascript/layout.js
+++ javascript/layout.js
@@ -22,22 +22,3 @@ if (btn) {
         }
     });
 }
-const layoutBtn = document.querySelector(".layout-btn");
-
-
-if (localStorage.getItem("layout") === "vertical") {
-    document.body.classList.add("vertical");
-}
-
-
-if (layoutBtn) {
-    layoutBtn.addEventListener("click", () => {
-        document.body.classList.toggle("vertical");
-
-        if (document.body.classList.contains("vertical")) {
-            localStorage.setItem("layout", "vertical");
-        } else {
-            localStorage.setItem("layout", "horizontal");
-        }
-    });
-}

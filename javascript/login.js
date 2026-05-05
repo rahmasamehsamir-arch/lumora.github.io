a19d7f6e48188572940e3435026d00e4a97c3457
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-05 22:33:43 +0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,43 +1,41 @@
-let form = document.querySelector('form');
+let form = document.querySelector("form");
 
 if(form){
-    form.addEventListener('submit', function(e){
-        e.preventDefault();
+form.addEventListener("submit", function(e){
+    e.preventDefault();
 
-        let input_email = document.querySelector('.in_mail');
-        let input_password = document.querySelector('.in_word');
+    let input_email = document.querySelector('.in_mail');
+    let input_password = document.querySelector('.in_word');
 
-        let email = input_email.value.trim();
-        let password = input_password.value.trim();
+    let email = input_email.value.trim();
+    let password = input_password.value.trim();
 
-        if(email === ""){
-            alert("Please enter an email");
-            return;
-        }
+    if(email === ""){
+        alert("Please enter an email");
+        return;
+    }
 
-        if(password === ""){
-            alert("Please enter a password");
-            return;
-        }
+    if(password === ""){
+        alert("Please enter a password");
+        return;
+    }
 
-        let data = localStorage.getItem(email);
+    let data = localStorage.getItem(email);
 
-        if(data === null){
-            alert("Please sign up first");
-            return;
-        }
+    if(data === null){
+        alert('Please sign up first');
+        return;
+    }
 
-        let user_data = JSON.parse(data);
+    let user_data = JSON.parse(data);
 
-    
-        if(user_data.password === password){
-            alert("Welcome back " + user_data.first_name);
-
-            localStorage.setItem('logged', email);
-
-            window.location.href = "../index.html";
-        } else {
-            alert("Incorrect password");
-        }
-    });
+    if(user_data.password === password){
+        alert('Welcome back ' + user_data.first_name);
+        localStorage.setItem('logged', email);
+        window.location.href = "../index.html";
+    } else {
+        alert('Incorrect password');
+    }
+});
 }
+

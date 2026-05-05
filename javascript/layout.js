992e34fdc5cb42ac0a726e8a11c69987b25f21c2
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-05 03:04:14 +0300

    Update layout.js

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,18 +1,11 @@
-
 const body = document.body;
 const modeBtn = document.querySelector(".mode");
 const layoutBtn = document.querySelector(".layout");
-
-
 if (localStorage.getItem("mode") === "dark") {
-    body.classList.add("dark");
-}
-
+    body.classList.add("dark");}
 if (localStorage.getItem("layout") === "vertical") {
     body.classList.add("vertical");
 }
-
-
 if (modeBtn) {
     modeBtn.addEventListener("click", () => {
         body.classList.toggle("dark");
@@ -20,22 +13,8 @@ if (modeBtn) {
         if (body.classList.contains("dark")) {
             localStorage.setItem("mode", "dark");
         } else {
-            localStorage.setItem("mode", "light");
+         localStorage.setItem("mode", "light");
         }
-    });
-}
-
-
+    });}
 if (layoutBtn) {
-    layoutBtn.addEventListener("click", () => {
-
-        body.classList.toggle("vertical");
-
-        if (body.classList.contains("vertical")) {
-            localStorage.setItem("layout", "vertical");
-        } else {
-            localStorage.setItem("layout", "horizontal");
-        }
-
-    });
-}
+    layoutBtn.addEventListener("click

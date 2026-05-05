cd18136dcb2556270ebe457eed1dd0977e6de2f6
Jana Safwat <janasafwat001@gmail.com>
2026-05-05 23:56:02 +0300

    update

--- javascript/signout.js
+++ javascript/signout.js
@@ -22,3 +22,30 @@ if(sign_out){
        
         sign_out.style.display = "none";
     }
+
+
+
+    let client = localStorage.getItem('logged');
+let sign_out = document.querySelector('.out');
+
+if(sign_out){
+
+    if(client){
+    
+        sign_out.style.display = "inline-block";
+
+   
+        sign_out.addEventListener("click", function(e){
+            e.preventDefault();
+            localStorage.removeItem('logged');
+            alert('Signed out successfully');
+
+            sign_out.style.display = "none";
+
+            window.location.href = "../index.html";
+        });
+
+    } else {
+       
+        sign_out.style.display = "none";
+    }

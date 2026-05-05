32e10c44a911f8f4e18f1ba5d546dd2f43e509dd
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-05 22:45:14 +0300

    Update signout.js

--- javascript/signout.js
+++ javascript/signout.js
@@ -2,23 +2,17 @@ let client = localStorage.getItem('logged');
 let sign_out = document.querySelector('.out');
 
 if(sign_out){
-
     if(client){
-    
         sign_out.style.display = "inline-block";
 
-   
         sign_out.addEventListener("click", function(e){
             e.preventDefault();
             localStorage.removeItem('logged');
             alert('Signed out successfully');
-
-            sign_out.style.display = "none";
-
             window.location.href = "../index.html";
         });
 
     } else {
-       
         sign_out.style.display = "none";
     }
+}

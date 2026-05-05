0aee568321fd57f54b77f83b8f85e03cbfbd8c17
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-05 22:16:48 +0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,38 +1,48 @@
+let form = document.querySelector('form');
 
-let form =document.querySelector('form')
 if(form){
-    form.addEventListener('submit',function(e){
+    form.addEventListener('submit', function(e){
         e.preventDefault();
-        let input_email =document.querySelector('.in_mail');
-        let input_password =document.querySelector('.in_word');
+
+        let input_email = document.querySelector('.in_mail');
+        let input_password = document.querySelector('.in_word');
 
         let email = input_email.value.trim();
         let password = input_password.value.trim();
-        if(email==="")
-        {   
+
+        // validation
+        if(email === ""){
             alert("Please enter an email");
             return;
         }
-        if(password==="")
-        {   
-            alert("Please enter an password");
+
+        if(password === ""){
+            alert("Please enter a password");
             return;
         }
-        let data=localStorage.getItem(email);
-        if(data===null)
-        {
-            alert('Please sign up first');
+
+        // get data
+        let data = localStorage.getItem(email);
+
+        if(data === null){
+            alert("Please sign up first");
             return;
         }
-        let user_data=JSON.parse(data);
-        if(user_data[4]===password)
-        {
-        alert('Welcome back '+user_data[0]);
-        localStorage.setItem('logged',email);
-        window.location.href='../index.html';
+
+        let user_data = JSON.parse(data);
+
+        // check password (array index 4)
+        let stored_password = user_data[4];
+        let first_name = user_data[0];
+
+        if(stored_password === password){
+            alert("Welcome back " + first_name);
+
+            localStorage.setItem('logged', email);
+
+            window.location.href = "../index.html";
+        } else {
+            alert("Incorrect password");
         }
-        else
-        {
-            alert('In correct password');
-        }
-    })};
+    });
+}

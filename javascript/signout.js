1dd12b8254bcd712b9d703febb42df0b1a4b2e3f
Amal-Dev24 <amalemara625@gmail.com>
2026-05-08 14:14:51 +0300

    Update signout.js

--- javascript/signout.js
+++ javascript/signout.js
@@ -1,22 +1,24 @@
 
 
 
-    let client = localStorage.getItem('logged');
+let client = localStorage.getItem('logged');
 let sign_out = document.querySelector('.out');
-
+let sign_in = document.querySelector('a[href='login.html']');
+let sign_up = document.querySelector('a[href='signup.html']');
 if(sign_out){
 
     if(client)
     {
-    
-        sign_out.style.display = "inline-block";
+        sign_out.style.display = "block";
+        sign_in.style.display = "none";
+        sign_up.style.display = "none";
         sign_out.addEventListener("click", function(e)
         {
             e.preventDefault();
             localStorage.removeItem('logged');
             alert('Signed out successfully');
 
-            sign_out.style.display = "none";
+            
 
             window.location.href = "../index.html";
         });
@@ -25,5 +27,7 @@ if(sign_out){
     else 
     {
         sign_out.style.display = "none";
+        sign_in.style.display = "inline-block";
+        sign_up.style.display = "inline-block";
     }
 }

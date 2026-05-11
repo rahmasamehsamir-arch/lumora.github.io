b4d0865dbc0337e85743c579564d08c104f26247
Amal-Dev24 <amalemara625@gmail.com>
2026-05-11 15:15:39 +0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -2,43 +2,28 @@ let users = JSON.parse(localStorage.getItem("users")) || [];
 let form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-
     let input_email = document.querySelector('.in_mail');
     let input_password = document.querySelector('.in_word');
-
     let email = input_email.value.trim();
     let password = input_password.value.trim();
-
     if (email === "") {
         alert("Please enter an email");
-        return;
-    }
-
+        return;}
     if (password === "") {
         alert("Please enter a password");
-        return;
-    }
-
-    if (users === null) {
+        return;}
+    if (users.length === 0) {
         alert('Please sign up first');
-        return;
-    }
-
+        return;}
     let dataOfUserThatLoggedIn = null;
-
     for (let i = 0; i < users.length; i++) {
-        if (users[i].email == email || users[i].password == password) {
+        if (users[i].email == email && users[i].password == password) {
             dataOfUserThatLoggedIn = users[i];
-            break;
-        }
-    }
-
+            break; } }
     if (dataOfUserThatLoggedIn != null) {
         alert('Welcome back ' + dataOfUserThatLoggedIn.first_name);
         localStorage.setItem('logged', email);
         window.location.href = "../index.html";
         return false;
     } else {
-        alert('Incorrect password or email');
-    }
-});
+        alert('Incorrect password or email');  }});

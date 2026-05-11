17643ebfec918722144f7d017b137c0d54306d3d
Amal-Dev24 <amalemara625@gmail.com>
2026-05-11 14:54:51 +0300

    Update signup.js

--- javascript/signup.js
+++ javascript/signup.js
@@ -23,7 +23,12 @@ form.addEventListener("submit", function (event) {
         alert('Please fill in all fields');
         return;
     }
-
+    if(/\d/.test(first){
+        alert("First name must contain letters only");
+    }
+     if(/\d/.test(last){
+        alert("Last name must contain letters only");
+    }
     if (!mail.includes('@') || !mail.includes('.')) {
         alert("Email is not valid");
         return;

609872333e82cb707fc9816f50f63ccc0eb98991
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-12 09:04:18 +0300

    Refactor menu and slideshow functions in index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,16 @@
-
 function openMenu() {
-  document.getElementById("sideWindow").style.right = "0";
+    document.getElementById("sideWindow").style.right = "0";
 }
-
 function closeMenu() {
-  document.getElementById("sideWindow").style.right = "-260px";
+    document.getElementById("sideWindow").style.right = "-260px";
 }
-
 let slides = document.querySelectorAll(".card img");
 let index = 0;
 setInterval(function () {
-  slides[index].classList.remove("active");
-  index++;
-  if (index >= slides.length) {
-    index = 0;
-  }
-  slides[index].classList.add("active");
+    slides[index].classList.remove("active");
+    index++;
+    if (index >= slides.length) {
+        index = 0;
+    }
+    slides[index].classList.add("active");
 }, 4000);

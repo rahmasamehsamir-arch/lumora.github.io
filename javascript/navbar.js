e93267f8a01d674da399365ca9f0483dc72b61a8
rahmasamehsamir-arch <rahmasamehsamir@gmail.com>
2026-05-12 09:05:40 +0300

    Refactor toggleMenu function and event listener

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,12 +1,7 @@
-
-
 function toggleMenu(event) {
   event.stopPropagation(); 
-
   document.getElementById("menu").classList.toggle("show");
 }
-
-
 window.addEventListener("click", function(e) {
   if (!e.target.closest(".user-menu")) {
     document.getElementById("menu").classList.remove("show");
